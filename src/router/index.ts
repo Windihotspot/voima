02aa8869dbd28ee943bc0c919c2b56b14f23ccae
windihotspot <boiwindihotspot@gmail.com>
2026-06-22 00:01:41 +0100

    web hash history

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Onboarding from '../views/Onboarding.vue'
 import WaitlistPage from '../views/WaitlistPage.vue'
 import ComplianceAssessment from '@/views/ComplianceAssessment.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
